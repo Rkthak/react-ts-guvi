a8ca7f8a7b109f3aa547c706b5e6ac2cd58925c9
Rkthak <rkon2609@gmail.com>
2026-04-18 15:43:57 +0530

    variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import Header from "./Components/Header";
 function App() {
-  const username: string = "Rak";
   return (
     <div>
       <Header />
-      <h1>{username}</h1>
     </div>
   );
 }
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,10 @@
 function Header() {
-  return <h1>header</h1>;
+  const name: string = "Raj";
+  let age: number = 23;
+  return (
+    <h1>
+      {name} {age}
+    </h1>
+  );
 }
 export default Header;
